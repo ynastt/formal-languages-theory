0d73070392f06d5c00e9233f5f8342caac4c5e7e
ynastt <yarovikovaanastasia1@gmail.com>
2022-09-16 20:03:54 +0300

    changes due to spaces in strings

--- main.go
+++ main.go
@@ -10,26 +10,26 @@ import (
 
 // построчное чтение из файла
 // заполнение массивов нетерминалов, терминалов и карты: нетерминал -> спсиок правил переписыванния
-func makeListOfTermForms() ([]string, []string, map[string][]string) {
+func parseTerms() ([]string, []string, map[string][]string) {
 	var nonTerms, terms []string
 	rules := make(map[string][]string)
-	file, err := os.Open("tests/test1.txt")
+	file, err := os.Open("tests/test3.txt")
 	if err != nil {
 		log.Fatalf("Error with openning file: %s", err)
 	}
 	fileScanner := bufio.NewScanner(file)
 	for fileScanner.Scan() {
 		if strings.Contains(fileScanner.Text(), "nonterminals") {
-			tmp := strings.Split(fileScanner.Text(), "=")[1]
+			tmp := strings.TrimSpace(strings.Split(fileScanner.Text(), "=")[1])
 			nonTerms = strings.Split(tmp, ",")
 			//fmt.Println(nonTerms[0:])
 		} else if strings.Contains(fileScanner.Text(), "terminals") {
-			tmp := strings.Split(fileScanner.Text(), "=")[1]
+			tmp := strings.TrimSpace(strings.Split(fileScanner.Text(), "=")[1])
 			terms = strings.Split(tmp, ",")
 			//fmt.Println(terms[0:])
 		} else if strings.Contains(fileScanner.Text(), "->") {
-			tmp := strings.Split(fileScanner.Text(), "-> ")
-			rules[tmp[0]] = append(rules[tmp[0]], tmp[1])
+			tmp := strings.Split(fileScanner.Text(), " -> ")
+			rules[tmp[0]] = append(rules[tmp[0]], strings.ReplaceAll(tmp[1], " ", ""))
 			fmt.Printf("%s - %s\n", tmp[0], rules[tmp[0]])
 		}
 	}
@@ -43,18 +43,40 @@ func makeListOfTermForms() ([]string, []string, map[string][]string) {
 	return terms, nonTerms, rules
 }
 
-/*func parseNonTerminals() {
+func makeListOfTermForms(nonTerms []string, rules map[string][]string) map[string][]string {
+	forms := make(map[string][]string)
+	//var curForm string
+	nt := strings.ReplaceAll(strings.Join(nonTerms, ""), " ", "")
+	//fmt.Println(nt)
+	/*for _, n := range nonTerms {
+
+		//for v := 0; v < len(val); v++ {
+		fmt.Printf("%s- %s\n", key, v)
+		curForm = strings.ReplaceAll(v, n, "")
+		//fmt.Printf("term form: %s\n", curForm)
+		forms[key] = append(forms[key], curForm)
+
+	}*/
+	for _, val := range rules {
+		for _, v := range val {
+			if strings.ContainsAny(v, nt) {
+
+			}
+		}
+	}
+	return forms
 }
 
-
-func EqClassesDivision() {
-}*/
+//func EqClassesDivision() {
+//}
 
 func main() {
-	terms, nonTerms, rules := makeListOfTermForms()
+	terms, nonTerms, rules := parseTerms()
 	fmt.Println("nonterms:")
 	fmt.Println(nonTerms[0:])
 	fmt.Println("terms:")
 	fmt.Println(terms[0:])
 	fmt.Println(rules)
+	//termForms := makeListOfTermForms(nonTerms, rules)
+	//fmt.Println(termForms)
 }
